498a4e47f2560a15a4d7189c7080a23da333b6f8
DanteSpard4 <132237129+DanteSpard4@users.noreply.github.com>
2023-10-01 21:42:58 -0500

    Initial commit

---
+++ src/main/java/com/calc/gtc/CalculaApplication.java
@@ -0,0 +1,13 @@
+package com.calc.gtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calc/gtc/CalculaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calc.gtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
