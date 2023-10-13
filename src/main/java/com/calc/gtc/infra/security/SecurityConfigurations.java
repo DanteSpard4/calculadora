7d235b114c028854ffe51914ebdb11fe5be3a613
DanteSpard4 <132237129+DanteSpard4@users.noreply.github.com>
2023-10-13 15:57:17 -0500

    a0.0.0.2

--- src/main/java/com/calc/gtc/infra/security/SecurityConfigurations.java
+++ src/main/java/com/calc/gtc/infra/security/SecurityConfigurations.java
@@ -1,17 +1,20 @@
 package com.calc.gtc.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurutyConfigurations {
-
-
+public class SecurityConfigurations {
+    @Autowired
+    private SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         //return httpSecurity.csrf() seguridad web
--- src/main/java/com/calc/gtc/infra/security/SecurityFilter.java
+++ src/main/java/com/calc/gtc/infra/security/SecurityFilter.java
@@ -1,4 +1,35 @@
 package com.calc.gtc.infra.security;
 
-public class SecurityFilter {
+import com.calc.gtc.domain.usuarios.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader!=null){
+            var token = authHeader.replace("bearer","");
+            var subject = tokenService.getSubject(token);
+            if(subject!=null){
+                var usuario = usuarioRepository.findByLogin(subject);
+                var auth = new UsernamePasswordAuthenticationToken(usuario,null,usuario.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+
+        filterChain.doFilter(request,response);
+    }
 }
