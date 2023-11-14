6173ecb9a44750730b3b65355bfed88d142220ce
DanteSpard4 <132237129+DanteSpard4@users.noreply.github.com>
2023-11-13 22:25:39 -0500

    a 0.0.11

--- src/main/java/com/calc/gtc/infra/congig/CorsConfig.java
+++ src/main/java/com/calc/gtc/infra/congig/CorsConfig.java
@@ -12,14 +12,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://pruebas-production-7ef0.up.railway.app:80")
+                .allowedOrigins("127.0.0.1:5500")
                 .allowedMethods("*");
-        registry.addMapping("/**")
-                .allowedOrigins("https://pruebas-production-7ef0.up.railway.app:80")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
-                .allowCredentials(true);
-
     }
 }
 
