de1ea8425e6d61ef69efd8230e82cad87a2fc9cb
DanteSpard4 <132237129+DanteSpard4@users.noreply.github.com>
2023-11-13 19:27:50 -0500

    a 0.0.1

--- src/main/java/com/calc/gtc/domain/gastos/GastosRepository.java
+++ src/main/java/com/calc/gtc/domain/gastos/GastosRepository.java
@@ -1,6 +1,8 @@
 package com.calc.gtc.domain.gastos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GastosRepository extends JpaRepository<Gastos,Long>{
 }
--- src/main/resources/db/migration/V2__create-tables.sql
+++ src/main/resources/db/migration/V2__create-tables.sql
@@ -1,7 +1,7 @@
 -- Tabla para almacenar información financiera
 CREATE TABLE Finanzas (
     FinanzasID INT PRIMARY KEY,
-    UsuarioID INT,
+    UsuarioID bigint,
     Salario DECIMAL(15, 2),
     OtrosIngresos DECIMAL(15, 2),
     CONSTRAINT FK_Finanzas_Usuario FOREIGN KEY (UsuarioID) REFERENCES usuarios(id)
@@ -10,7 +10,7 @@ CREATE TABLE Finanzas (
 -- Tabla para gastos
 CREATE TABLE Gastos (
     GastoID INT PRIMARY KEY,
-    UsuarioID INT,
+    UsuarioID bigint,
     Categoria VARCHAR(50),
     Monto DECIMAL(15, 2),
     Banco VARCHAR(50),
@@ -18,24 +18,21 @@ CREATE TABLE Gastos (
     Descripcion VARCHAR(255),
     CONSTRAINT FK_Gastos_Usuario FOREIGN KEY (UsuarioID) REFERENCES usuarios(id)
 );
-CREATE TABLE suscripciones{
-    ID INT PRIMARY KEY,
-    
-}
+
 CREATE TABLE Ingreso(
     IngresoID INT PRIMARY KEY,
-    UsuarioID INT,
-    Monto DECIMAL(15,2)
+    UsuarioID bigint,
+    Monto DECIMAL(15,2),
     Fecha DATE,
     Banco VARCHAR(50),
     Descripcion VARCHAR(255),
-    CONSTRAINT FK_Ingreso_Usuario FOREIGN (UsuarioID) REFERENCES usuarios(id)
+    CONSTRAINT FK_Ingreso_Usuario FOREIGN KEY(UsuarioID) REFERENCES usuarios(id)
 );
 
 -- Tabla para ahorros e inversiones
 CREATE TABLE AhorrosInversiones (
     AhorroInversionID INT PRIMARY KEY,
-    UsuarioID INT,
+    UsuarioID bigint,
     Tipo VARCHAR(50), -- Cuenta de ahorro, acciones, bonos, etc.
     Saldo DECIMAL(15, 2),
     CONSTRAINT FK_AhorrosInversiones_Usuario FOREIGN KEY (UsuarioID) REFERENCES usuarios(id)
@@ -44,7 +41,7 @@ CREATE TABLE AhorrosInversiones (
 -- Tabla para historial de transacciones
 CREATE TABLE Transacciones (
     TransaccionID INT PRIMARY KEY,
-    UsuarioID INT,
+    UsuarioID bigint,
     Tipo VARCHAR(50), -- Ingreso o gasto
     Monto DECIMAL(15, 2),
     Fecha DATE,
@@ -55,7 +52,7 @@ CREATE TABLE Transacciones (
 -- Tabla para deudas y préstamos
 CREATE TABLE DeudasPrestamos (
     DeudaPrestamoID INT PRIMARY KEY,
-    UsuarioID INT,
+    UsuarioID bigint,
     Tipo VARCHAR(50), -- Préstamo, tarjeta de crédito, etc.
     SaldoActual DECIMAL(15, 2),
     CuotaMensual DECIMAL(15, 2),
@@ -65,7 +62,7 @@ CREATE TABLE DeudasPrestamos (
 -- Tabla para registros sesiones
 CREATE TABLE RegistroSesiones (
     SesionID INT PRIMARY KEY,
-    UsuarioID INT,
+    UsuarioID bigint,
     FechaHoraInicio DATETIME,
     FechaHoraFin DATETIME,
     CONSTRAINT FK_RegistroSesiones_Usuario FOREIGN KEY (UsuarioID) REFERENCES usuarios(id)
