8756ddbf00022653aa46a9c61124a0b26d5661a0
DanteSpard4 <132237129+DanteSpard4@users.noreply.github.com>
2023-11-14 00:54:46 -0500

    a 0.0.12

--- src/main/java/com/calc/gtc/controller/CorsConfig.java
+++ src/main/java/com/calc/gtc/controller/CorsConfig.java
@@ -1,8 +1,7 @@
-package com.calc.gtc.infra.congig;
+package com.calc.gtc.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
