efdcadd721229b0a4a3ac8061e7cca2c5ac60e8f
DanteSpard4 <132237129+DanteSpard4@users.noreply.github.com>
2023-11-15 00:36:45 -0500

    a 0.0.2

--- src/main/java/com/calc/gtc/infra/security/SecurityConfigurations.java
+++ src/main/java/com/calc/gtc/infra/security/SecurityConfigurations.java
@@ -49,7 +49,8 @@ public class SecurityConfigurations implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5501")
-                .allowedOrigins("https://pruebas-production-7ef0.up.railway.app/")
+                .allowedOrigins("https://pruebas-production-7ef0.up.railway.app")
+                .allowedOrigins("http://127.0.0.1:5173")
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
