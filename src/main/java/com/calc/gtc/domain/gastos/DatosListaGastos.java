ec4e8f4e08a03ef8695574adc3640ad8943557d8
DanteSpard4 <132237129+DanteSpard4@users.noreply.github.com>
2024-01-04 22:03:55 -0500

    v 0.0.5 se añadio tipo a respuesta de datoslistagatos

--- src/main/java/com/calc/gtc/domain/gastos/DatosListaGastos.java
+++ src/main/java/com/calc/gtc/domain/gastos/DatosListaGastos.java
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record DatosListaGastos(Long gastoID, String categoria, BigDecimal monto, String banco, LocalDate fecha, String descripcion,String razon) {
+public record DatosListaGastos(Long gastoID, String categoria, BigDecimal monto, String banco, LocalDate fecha, String descripcion,String razon,Boolean tipo) {
 
     public DatosListaGastos(Gastos gasto){
-        this(gasto.getGastoID(),gasto.getCategoria(),gasto.getMonto(), gasto.getBanco(),gasto.getFecha(), gasto.getDescripcion(),gasto.getRazon());
+        this(gasto.getGastoID(),gasto.getCategoria(),gasto.getMonto(), gasto.getBanco(),gasto.getFecha(), gasto.getDescripcion(),gasto.getRazon(),gasto.getTipo());
     }
 
 }
